60fd6c9609dd35228b3d72dee5ab6276b32c952a
Nivya Jomichan <nivya1997@gmail.com>
2024-03-26 01:59:24 -0400

    Navbar and Intro (Raw)

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hi1 I Am</span>
+                <span>Nivya Jomichan</span>
+                <span>Frontend Developer with high
+                level of experience in web designing
+                and development, producting the
+                Quality work </span>
+
+            </div>
+        <button className="button i-button">Hire me</button>
+        </div>
+        <div className="i-right"></div>
+    </div>
+  )
+}
+
+export default Intro
